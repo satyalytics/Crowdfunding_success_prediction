537dbac469ebc796a74324dec7380741a90961ff
satyalytics <satyalytics@gmail.com>
2021-04-02 11:39:35 +0530

    structured project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To predict your campaign's success on kickstarter',
+    author='satyalytics',
+    license='MIT',
+)
